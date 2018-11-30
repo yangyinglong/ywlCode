25aa05b663e105858feb4e849316ce69923a0cba
yangweili <yangwl1999@foxmial.com>
2018-11-30 14:29:14 +0800

    ...

--- stuSystem/stuSystem.c
+++ stuSystem/stuSystem.c
@@ -1,20 +1,78 @@
 // 学生管理系统
+#include<stdio.h>
+
+void printHead();
+void add();         //增加一条记录
+void delete();      //删除一条记录
+
+struct Student
+{
+	char idNumber[50];
+	char name[50];
+	int sex;	
+};
 
 
 int main()
 {
-	while() 
+	char order;
+	int sign = 1;
+	while(sign == 1) 
 	{
-		// 接受命令 
-		switch()
+		scanf("%c", &order);
+		switch(order)
 		{
-			case:
-				// 执行相应的命令代码函数
-			case:
+			case 'a': add();   //增加一条记录
+			
+				
+			case 'd': delete();  //根据学号删除一条记录
+
+
+			case 'f':    //根据性别查询符合条件的所有学生的信息并打印出来
+
+
+			case 'p':    //打印所有学生信息
+
+
+			case 'q':    //退出系统
 
 		}
-	}
+	} 
+	return 0;  
 }
 
 
 // 实现相应的函数
+
+
+/*
+  在终端打印出
+  ---------------------------------
+  |IdNumber       |Name   |Sex    |
+ */
+void printHead()
+{
+	for (int i = 0; i < 33; ++i)
+	{
+		printf("-");
+	}
+	printf("\n");
+	printf("|Idnumber\t|name\t|sex\t|\n");
+	
+}
+
+void add()
+{
+
+
+
+
+}
+
+void delete()
+{
+
+
+
+
+}
