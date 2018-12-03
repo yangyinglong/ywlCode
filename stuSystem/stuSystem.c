b60942e2f7d6d7eb63013780773fc9000aa09a9d
yangweili <yangwl1999@foxmial.com>
2018-12-03 15:26:41 +0800

    ...

--- stuSystem/stuSystem.c
+++ stuSystem/stuSystem.c
@@ -1,40 +1,50 @@
 // 学生管理系统
-#include<stdio.h>
-
-void printHead();
-void add();         //增加一条记录
-void delete();      //删除一条记录
+#include<stdio.h>    
+#include<string.h>
 
 struct Student
 {
 	char idNumber[50];
 	char name[50];
-	int sex;	
+	char sex[2];	
 };
+
+//定义学生数量
+int counts = 0;
+
+void printHead();
+void add(struct Student stu[100]);         //增加一条记录
+void delete(struct Student stu[100]);      //删除一条记录
+void search(struct Student stu[100]);      //根据性别查询符合条件的所有学生的信息并打印出来
+void printall(struct Student stu[100]);    //打印所有学生信息
+
 
 
 int main()
 {
+	struct Student stu[100];
 	char order;
 	int sign = 1;
 	while(sign == 1) 
 	{
+		printf("a-add\nd-delete\nf-search\np-printall\nq-exit\n");
+		printf("please enter your order:");
 		scanf("%c", &order);
 		switch(order)
 		{
-			case 'a': add();   //增加一条记录
+			case 'a': add();        //增加一条记录
 			
 				
-			case 'd': delete();  //根据学号删除一条记录
+			case 'd': delete();     //根据学号删除一条记录
 
 
-			case 'f':    //根据性别查询符合条件的所有学生的信息并打印出来
+			case 'f': search();     //根据性别查询符合条件的所有学生的信息并打印出来
 
 
-			case 'p':    //打印所有学生信息
+			case 'p': printall();   //打印所有学生信息
 
 
-			case 'q':    //退出系统
+			case 'q': break;        //退出系统
 
 		}
 	} 
@@ -71,6 +81,23 @@ void add()
 
 void delete()
 {
+
+
+
+
+}
+
+void search()
+{
+
+
+
+
+}
+
+void printall()
+{
+
 
 
 
